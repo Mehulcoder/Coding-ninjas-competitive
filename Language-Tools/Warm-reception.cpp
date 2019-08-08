24995d3dbd2cfc54f9f315a31d60f8f32e2aa05a
Mehulcoder <mehul355180@gmail.com>
2019-08-09 04:33:36 +0530

    Adding solutions to Language Tools section

---
+++ Language-Tools/Warm-reception.cpp
@@ -0,0 +1,26 @@
+/*
+
+				Name: Mehul Chaturvedi
+				IIT-Guwahati
+
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main( int argc , char ** argv )
+{
+	ios_base::sync_with_stdio(false) ; 
+	cin.tie(NULL) ; 
+	
+
+
+
+	return 0 ; 
+
+
+
+}
