5985e940a434f5b45a6c19d3c416cac5b8fa904c
Mehulcoder <mehul355180@gmail.com>
2019-08-31 19:34:40 +0530

    Added more problems

--- Dynamic programming 2/Charlie.cpp
+++ Dynamic programming 2/Charlie.cpp
@@ -21,8 +21,60 @@ Sample Output
 19000 
 */
 #include <bits/stdc++.h>
-
 using namespace std;
+typedef pair<int, int> pii;
+#define f first
+#define s second
+
+int go(vector<pii> pilots, int i, int ncap, int nass, int*** mem){
+	if (i>=pilots.size())
+	{
+		return 0;
+	}
+
+
+	if (ncap == 0 && nass == 0)
+	{
+		return 0;
+	}
+
+	if (pilots.size()-i<ncap+nass)
+	{
+		return 0;
+	}
+	// if (i==0)
+	// {
+	// 	cout<<ncap<<" "<<nass;
+	// }
+	// return 0;
+
+	if(mem[i][ncap][nass] != -1){
+		return mem[i][ncap][nass];
+	}
+
+	int c1 = INT_MAX;
+	int c2 = INT_MAX;
+
+	if (nass<ncap)
+	{
+		c1 = pilots[i].f + go(pilots, i+1, ncap-1, nass, mem);
+		mem[i+1][ncap-1][nass] = go(pilots, i+1, ncap-1, nass, mem);
+	}
+
+	if (nass>=1)
+	{
+		c2 = pilots[i].s + go(pilots, i+1, ncap, nass-1, mem);
+		mem[i+1][ncap][nass-1] = go(pilots, i+1, ncap, nass-1, mem);	
+	}
+
+	//cout << "i "<<i<<" ncap "<<ncap<<" nass "<<nass << '\n';
+	mem[i][ncap][nass] = min(c1, c2);
+	return min(c1, c2);
+
+
+
+}
+
 
 
 int main( int argc , char ** argv )
@@ -30,11 +82,53 @@ int main( int argc , char ** argv )
 	ios_base::sync_with_stdio(false) ; 
 	cin.tie(NULL) ; 
 	
+	int n;
+	cin>>n;
+
+	vector<pii> v1(n, make_pair(0, 0));
+	for (int i = 0; i < n; ++i)
+	{
+		int a, b;
+		cin>>a>>b;
+		v1[i] = make_pair(a,b);
+	}
+
+	int*** mem = new int**[n+2];
+	for (int i = 0; i < n+2; ++i)
+	{
+		mem[i] = new int*[n/2+3];
+		for (int j = 0; j < n/2+3; ++j)
+		{
+			mem[i][j] = new int[n/2+3];
+		}
+	}
+	//int mem[100][100][100];
 
 
+	//memset(mem, -1, (n+2)*(n/2+3)*(n/2+3)*sizeof(int));
+	for (int i = 0; i < n+2; ++i)
+	{
+		for (int j = 0; j < n/2+3; ++j)
+		{
+			for (int k = 0; k < n/2+3; ++k)
+			{
+				mem[i][j][k] = -1;
+			}
+		}
+	//	cout << '\n';
+	}
+
+	// for (int i = 0; i < n+2; ++i)
+	// {
+	// 	for (int j = 0; j < n/2+3; ++j)
+	// 	{
+	// 		cout << mem[i][j][0] << ' ';
+	// 	}
+	// 	cout << '\n';
+	// }
+
+
+	cout << go(v1, 0, n/2, n/2, mem) << '\n';
 
 	return 0 ; 
-
-
-
 }
