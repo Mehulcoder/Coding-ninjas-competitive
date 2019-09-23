9a9322e2ba812e2deff3b7c5299726f616fe3715
Mehulcoder <mehul355180@gmail.com>
2019-09-23 22:29:27 +0530

    Added more problems after midsems

--- Segment Trees/Minimuminsub.cpp
+++ Segment Trees/Minimuminsub.cpp
@@ -38,12 +38,67 @@ Sample Output :
 
 using namespace std;
 
-int go(vector<int> v, char x, int a, int b){
-
-
+int query(int* tree, int start, int end, int treeNode, int left, int right){
 	
+	//Completely out
+	if (left>end || right<start)
+	{
+		return INT_MAX;
+	}
+
+	//Completely inside
+	if (start>=left && end<=right)
+	{
+		return tree[treeNode];
+	}
+
+	//Partially inside
+	int mid = (start+end)/2;
+
+	int l = query(tree, start, mid, 2*treeNode+1, left, right);
+	int r= query(tree, mid+1, end, 2*treeNode+2, left, right);
+
+	return min(l, r);
 }
 
+
+void update(int* arr, int* tree, int start, int end, int treeNode, int idx, int value){
+	int mid = (start+end)/2;
+
+	if (start == end)
+	{
+		arr[idx] = value;
+		tree[treeNode] = value;
+		return;
+	}
+
+	if (idx<=mid)
+	{
+		update(arr, tree, start, mid, 2*treeNode+1, idx, value);
+	}else{
+
+		update(arr, tree, mid+1, end, 2*treeNode+2, idx, value);
+	}
+
+	tree[treeNode] = min(tree[2*treeNode+1], tree[2*treeNode+2]);
+}
+
+
+void create(int* arr, int* tree, int start, int end, int treeNode){
+	if (start == end)
+	{
+		tree[treeNode] = arr[start];
+		return;
+	}
+
+	int mid = (start+end)/2;
+
+	create(arr, tree, start, mid, 2*treeNode+1);
+	create(arr, tree, mid+1, end, 2*treeNode+2);
+
+	tree[treeNode] = min(tree[2*treeNode+1], tree[2*treeNode+2]);
+	return;
+}
 
 int main( int argc , char ** argv )
 {
@@ -52,22 +107,34 @@ int main( int argc , char ** argv )
 	
 	int n, q;
 	cin>>n>>q;
-	vector<int> v(n, 0);
 
+	int* arr = new int[n];
 	for (int i = 0; i < n; ++i)
 	{
-		cin>>v[i];
+		cin>>arr[i];
 	}
 
-	for (int i = 0; i < q; ++i)
-	{
-		char x;
-		int a;
-		int b;
-		cin>>x>>a>>b;
-		cout << go(v, x, a, b) << '\n';
+	int* tree = new int[4*n];
+
+	create(arr, tree, 0, n-1, 0);
+
+	while(q--){
+		char a;
+		int b, c;
+		cin>>a>>b>>c;
+
+		if (a=='q')
+		{
+			cout << query(tree, 0, n-1, 0, b-1, c-1) << '\n';
+		}else{
+			update(arr, tree, 0, n-1, 0, b-1, c);
+		}
 
 	}
 
-	return 0 ;
+
+	return 0 ; 
+
+
+
 }
