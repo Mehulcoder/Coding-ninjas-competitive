17daf58fa85b315b839d8602e5c34e122366b596
Mehulcoder <mehul355180@gmail.com>
2019-10-12 01:51:41 +0530

    Added more cool stuff

--- Tries/maximumxorsub.cpp
+++ Tries/maximumxorsub.cpp
@@ -23,30 +23,6 @@ Sample Output
 7
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-typedef unordered_map<int, int> umapii;
-typedef unordered_map<int, bool> umapib;
-typedef unordered_map<string, int> umapsi;
-typedef unordered_map<string, string> umapss;
-typedef map<string, int> mapsi;
-typedef map<pair<int, int>, int> mappiii;
-typedef map<int, int> mapii;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef unordered_set<int> useti;
-
-#define uset unordered_set
-#define it iterator
-#define mp make_pair
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define f first
-#define s second
-#define MOD 1000000007
-
 #include<bits/stdc++.h> 
 using namespace std; 
   
