760d454d1680f944656c39180a2ad4a980a6f05d
Bhavya Tyagi <55448429+bhavyatyagi@users.noreply.github.com>
2020-08-25 20:54:05 +0530

    Update bfs.cpp
    Modified the solution as per question (Added disconnected graphs)!

--- Graphs1/bfs.cpp
+++ Graphs1/bfs.cpp
@@ -13,6 +13,9 @@ Note : Take graph input in adjacency matrix.
 Input Format :
 Line 1: Two Integers V and E (separated by space)
 Next 'E' lines, each have two space-separated integers, 'a' and 'b', denoting that there exists an edge between Vertex 'a' and Vertex 'b'.
+Note : 1. Take graph input in the adjacency matrix.
+       2. Handle for Disconnected Graphs as well
+Input Format :
 Output Format :
 BFS Traversal (separated by space)
 Constraints :
@@ -26,6 +29,99 @@ Sample Input 1:
 2 3
 Sample Output 1:
 0 1 3 2
+*/
+
+
+
+/*
+					Contributer: Bhavya Tyagi
+					Thapar Institute of Engineering & Technology, Patiala
+					Added The Modified code after the question includes Disconnected Graphs too
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void printBFS(int **edges,int n, int j,bool *visited)
+{
+	
+	queue <int> q;
+	q.push(j);
+	visited[j]=true;
+	while(!q.empty())
+	{
+		int currentEdge=q.front();
+		cout<<currentEdge<<" ";
+		q.pop();
+		for(int i=0;i<n;i++)
+		{
+			if(i==currentEdge)
+			continue;
+			if(edges[currentEdge][i]&&!visited[i])
+			{
+				q.push(i);
+				visited[i]=true;
+			}
+			
+		}
+	}
+}
+
+void BFS(int **edges,int n)
+{
+	bool *visited=new bool [n];
+	
+	for(int i=0;i<n;i++)
+	visited[i]=false;
+	
+	for(int i=0;i<n;i++)
+	{
+		if(!visited[i])
+		printBFS(edges,n,i,visited);
+	}
+	delete[]visited;
+}
+
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    
+    int **edges=new int*[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        edges[i]=new int[n];
+        for(int j=0;j<n;j++)
+        edges[i][j]=0;
+    }
+    for(int i=0;i<e;i++)
+    {
+        int f,s;
+        cin>>f>>s;
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+    bool *visited=new bool[n];
+    
+    for(int i=0;i<n;i++)
+    visited[i]=false;
+    
+
+	BFS(edges,n);
+
+    
+    return 0;
+}
+
+
+
+
+/*
+
+			Old code without Disconnected Graphs (Only 50% Testcase Passes with the old code)
+			by Mehul Chaturvedi
 */
 
 #include <bits/stdc++.h>
